d2e764b7f504e2aa2ccb99183fa57483c6bbe765
Sebastiaan <sebastiaan@avido.nl>
2020-07-07 16:24:17 +0200

    Address endpoint (unfinished)

---
+++ src/Endpoints/Addresses.php
@@ -0,0 +1,22 @@
+<?php
+namespace Budgetlens\PostNLApi\Endpoints;
+
+/**
+ * Addresses Endpoint
+ * @see https://developer.postnl.nl/browse-apis/addresses/
+ * Class Addresses
+ * @package Budgetlens\PostNLApi\Endpoints
+ */
+
+class Addresses extends AbstractEndpoint
+{
+    public function getAddressByPostalcodeAndHouseNumber(string $postalCode, string $houseNumber)
+    {
+        $response = $this->client->get('/address/sequence/v1/postalcode', [
+            'postalcode' => $postalCode,
+            'housenumber' => $houseNumber
+        ]);
+        print_r($response->getBody()->json());
+        exit;
+    }
+}
