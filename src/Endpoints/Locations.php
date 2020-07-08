3810b94d26a5218d632a0432d29d6cbdda1894ad
Sebastiaan <sebastiaan@avido.nl>
2020-07-08 10:39:55 +0200

    Cleanup old code

--- src/Endpoints/Locations.php
+++ src/Endpoints/Locations.php
@@ -10,27 +10,15 @@ namespace Budgetlens\PostNLApi\Endpoints;
 
 class Locations extends AbstractEndpoint
 {
+    /**
+     * Get Nearest Locations
+     * @param array $data
+     * @return mixed
+     */
     public function nearestLocations(array $data = [])
     {
         return $this->createRequest('Budgetlens\PostNLApi\Messages\Requests\NearestLocationsRequest');
     }
 
-//
-//    public function getNearestLocations(
-//        string $postalCode,
-//        string $countryCode = "NL",
-//        array $deliveryOptions = ['PG'],
-//        string $street = null,
-//        int $houseNumber = null,
-//        \DateTime $deliveryDate = null,
-//        \DateTime $openingTime = null
-//    ) {
-//        $response = $this->client->get('')
-//        $response = $this->client->get('/address/sequence/v1/postalcode', [
-//            'postalcode' => $postalCode,
-//            'housenumber' => $houseNumber
-//        ]);
-//        print_r($response->getBody()->json());
-//        exit;
-//    }
+
 }
