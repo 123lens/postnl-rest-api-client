83bcbfb0c9668eff0c9e886c1dc51f5a9f472932
Sebastiaan <sebastiaan@avido.nl>
2020-07-17 09:38:51 +0200

    Updated exception handling

---
+++ src/Exceptions/ApiException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Budgetlens\PostNLApi\Exceptions;
+
+/**
+ * API Returned 'Fault' error
+ * Class ErrorException
+ * @package Budgetlens\PostNLApi\Exceptions
+ */
+
+class ApiException extends \Exception
+{
+}
---
+++ src/Exceptions/CifDownException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Budgetlens\PostNLApi\Exceptions;
+
+/**
+ * API Returned 'Fault/Reason' error
+ * Class ErrorException
+ * @package Budgetlens\PostNLApi\Exceptions
+ */
+
+class CifDownException extends \Exception
+{
+}
