abd4c8a0902874423d70cc647fda82a345c22565
Sebastiaan <sebastiaan@avido.nl>
2020-07-17 15:50:32 +0200

    Added labelling endpoint

---
+++ src/Endpoints/Labelling.php
@@ -0,0 +1,37 @@
+<?php
+namespace Budgetlens\PostNLApi\Endpoints;
+
+/**
+ * Labelling Endpoint
+ * @see https://developer.postnl.nl/browse-apis/send-and-track/labelling-webservice/
+ * Class Labelling
+ * @package Budgetlens\PostNLApi\Endpoints
+ */
+
+class Labelling extends AbstractEndpoint
+{
+    /**
+     * Generate Barcode Global Pack (EPS) Shipments
+     * @param array $data
+     * @return mixed
+     */
+    public function generateLabel(array $data = [])
+    {
+        return $this->createRequest(
+            'Budgetlens\PostNLApi\Messages\Requests\Labelling\GenerateLabelRequest',
+            $data
+        );
+    }
+
+    public function generateLabelWithoutConfirm(array $data = [])
+    {
+        $data = array_merge($data, [
+            'confirm' => true
+        ]);
+
+        return $this->createRequest(
+            'Budgetlens\PostNLApi\Messages\Requests\Labelling\GenerateLabelRequest',
+            $data
+        );
+    }
+}
