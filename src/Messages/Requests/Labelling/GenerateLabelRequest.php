d2d3a6178192ec4acfc525a07b3a748e3b242189
Sebastiaan <sebastiaan@avido.nl>
2020-08-13 08:43:47 +0200

    Added sendRequest

--- src/Messages/Requests/Labelling/GenerateLabelRequest.php
+++ src/Messages/Requests/Labelling/GenerateLabelRequest.php
@@ -16,11 +16,12 @@ namespace Budgetlens\PostNLApi\Messages\Requests\Labelling;
  */
 
 use Budgetlens\PostNLApi\Entities\Customer;
-use Budgetlens\PostNLApi\Messages\Requests\Barcode\AbstractGenerateBarcodeRequest;
+use Budgetlens\PostNLApi\Messages\Requests\AbstractRequest;
 use Budgetlens\PostNLApi\Messages\Requests\Contracts\MessageInterface;
 use Budgetlens\PostNLApi\Messages\Requests\Contracts\RequestInterface;
+use Budgetlens\PostNLApi\Messages\Responses\Laelling\GenerateLabelResponse;
 
-class GenerateLabelRequest extends AbstractGenerateBarcodeRequest implements RequestInterface, MessageInterface
+class GenerateLabelRequest extends AbstractRequest implements RequestInterface, MessageInterface
 {
     /**
      * Get Customer
@@ -73,22 +74,56 @@ class GenerateLabelRequest extends AbstractGenerateBarcodeRequest implements Req
     }
 
     /**
+     * Get Confirm Flag.
+     * Confirms shipment at postnl on label request
+     * If this value is false you need to manually confirm each shipment @ postnl
+     * @return bool
+     */
+    public function getConfirm(): bool
+    {
+        return (bool)$this->getParameter('confirm');
+    }
+
+    /**
+     * Set Confirm Flag
+     * Confirms shipment at postnl on label request
+     * If this value is false you need to manually confirm each shipment @ postnl
+     * @param bool $flag
+     * @return GenerateLabelRequest
+     */
+    public function setConfirm(bool $flag)
+    {
+        return $this->setParameter('confirm', $flag);
+    }
+
+    /**
      * Get Data
      * @return array
      */
     public function getData(): array
     {
         $this->validate(
-            'customer_code',
-            'customer_number'
+            'customer',
+            'shipments'
         );
-        $this->setType('3S');
-        // define serie.
-        if (strlen($this->getCustomerCode()) > 3) {
-            $this->setSerie(self::NL_BARCODE_SERIE_SHORT);
-        } else {
-            $this->setSerie(self::NL_BARCODE_SERIE_LONG);
-        }
         return parent::getData();
+    }
+
+    public function sendData(array $data = [])
+    {
+        $response = $this->client->request(
+            'POST',
+            '/shipment/v2_2/label',
+            [
+                'body' => json_encode($data),
+                'query' => [
+                    'confirm' => $this->getConfirm()
+                ],
+                'headers' => [
+                    'Content-Type' => 'application/json'
+                ]
+            ]
+        );
+        return $this->response = new GenerateLabelResponse($this, $response->getBody()->json());
     }
 }
