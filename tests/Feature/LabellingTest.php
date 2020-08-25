78c11cffdb1696690d18ad93e75112159b2b05b5
Sebastiaan <sebastiaan@avido.nl>
2020-08-25 15:41:49 +0200

    Added smart return label test

--- tests/Feature/LabellingTest.php
+++ tests/Feature/LabellingTest.php
@@ -725,7 +725,7 @@ class LabellingTest extends TestCase
         $barcode = '3STBJG243556390';
         $customer = $this->getCustomerEntity();
 
-        $request = $this->getClient()->labelling()->generateLabelWithoutConfirm();
+        $request = $this->getClient('Labelling/GenerateLabelSmartReturnLabelSuccess.json')->labelling()->generateLabelWithoutConfirm();
         $request->setPrinter('GraphicFile|PDF');
         $request->setCustomer($customer);
         $request->addShipment((new Shipment())
