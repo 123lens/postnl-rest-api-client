8a8786946c482f23a9a4e77e9994c77fe980ea46
Sebastiaan <sebastiaan@avido.nl>
2020-08-25 15:27:48 +0200

    Extra@Home COD Test

--- tests/Feature/LabellingTest.php
+++ tests/Feature/LabellingTest.php
@@ -495,7 +495,7 @@ class LabellingTest extends TestCase
         $barcode = '3STBJG243556390';
         $customer = $this->getCustomerEntity();
 
-        $request = $this->getClient()->labelling()->generateLabelWithoutConfirm();
+        $request = $this->getClient('Labelling/GenerateLabelExtraAtHomeCODSuccess.json')->labelling()->generateLabelWithoutConfirm();
         $request->setPrinter('GraphicFile|PDF');
         $request->setCustomer($customer);
         $request->addShipment((new Shipment())
