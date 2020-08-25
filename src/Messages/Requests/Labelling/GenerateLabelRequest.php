c094938029439e5493292ad94115c62064fa2676
Sebastiaan <sebastiaan@avido.nl>
2020-08-25 15:11:10 +0200

    Added delivery on demand test

--- src/Messages/Requests/Labelling/GenerateLabelRequest.php
+++ src/Messages/Requests/Labelling/GenerateLabelRequest.php
@@ -138,7 +138,7 @@ class GenerateLabelRequest extends AbstractLabellingRequest implements RequestIn
             ]
         );
 
-//        $filename = "/Users/sebastiaan/Projects/123 Lens/123lens-Opensource-Packages/postnl-rest-api/tests/Mocks/Labelling/GenerateLabelSameDaySuccess.json";
+//        $filename = "/Users/sebastiaan/Projects/123 Lens/123lens-Opensource-Packages/postnl-rest-api/tests/Mocks/Labelling/GenerateLabelDeliveryOnDemandSuccess.json";
 //        file_put_contents($filename, $response->getBody()->getContents());
 //        die("");
         return $this->response = new GenerateLabelResponse($this, $response->getBody()->json());
--- tests/Feature/LabellingTest.php
+++ tests/Feature/LabellingTest.php
@@ -261,7 +261,7 @@ class LabellingTest extends TestCase
         $barcode = '3STBJG243556388';
         $customer = $this->getCustomerEntity();
 
-        $request = $this->getClient()->labelling()->generateLabelWithoutConfirm();
+        $request = $this->getClient('Labelling/GenerateLabelDeliveryOnDemandSuccess.json')->labelling()->generateLabelWithoutConfirm();
         $request->setPrinter('GraphicFile|PDF');
         $request->setCustomer($customer);
         $request->addShipment((new Shipment())
